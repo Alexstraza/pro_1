ddfaf2ee488b62ac7b2b01f0219105e3eed0e3b0
Alexstraza <18281@ma-web.nl>
2015-08-31 15:11:44 +0200

    Updated the project files.

---
+++ les1/script.js
@@ -0,0 +1,17 @@
+window.addEventListener("load", function(){
+
+var counter = 0;
+var images = ["chester.jpg", "dubai.jpg", "florida.jpg", "halifax.jpg", "singapore.jpg"];
+            
+var window = document.getElementById("window");
+    window.innerHTML = 
+     
+window.addEventListener("click", function() {
+    
+  counter++;
+  console.log(images[counter]);
+    
+    
+});
+});                        
+                        
