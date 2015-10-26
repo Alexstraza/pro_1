884dd20dc2c67d9ed0585f789deb75a47c20223b
Alexstraza <18281@ma-web.nl>
2015-10-26 14:13:04 +0100

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- les1/script.js
+++ les1/script.js
@@ -3,13 +3,19 @@ window.addEventListener("load", function(){
 var counter = 0;
 var images = ["chester.jpg", "dubai.jpg", "florida.jpg", "halifax.jpg", "singapore.jpg"];
             
-var window = document.getElementById("window");
-    window.innerHTML = 
+var galleryImage = document.getElementById("galleryImage");
+    
+    
      
 window.addEventListener("click", function() {
     
   counter++;
-  console.log(images[counter]);
+  var url = images[counter];
+    galleryImage.src = "img/" + url;
+    
+    if (counter == 4) {
+        counter = -1;
+    }
     
     
 });
